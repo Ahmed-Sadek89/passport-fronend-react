af0b41b6874cbd43c11371060a5ca5f3d41bbfa1
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-07-14 22:41:46 +0200

    last

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       // https://file-api-sadek.herokuapp.com/auth/logout
       // https://file-api-sadek.herokuapp.com/auth/login/success
       // http://localhost:5000/auth/login/success
-       fetch("http://localhost:5000/auth/login/success", {
+       fetch("https://file-api-sadek.herokuapp.com/auth/login/success", {
         method: "GET",
         credentials: "include",
         headers: {
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,8 +7,8 @@ const Header = ({user}) => {
     console.log('i am header component');
     console.log(user);
     const logout = () => {
-        //window.open('https://file-api-sadek.herokuapp.com/auth/logout', '_self')
-        window.open('http://localhost:5000/auth/logout', '_self')
+        window.open('https://file-api-sadek.herokuapp.com/auth/logout', '_self')
+        //window.open('http://localhost:5000/auth/logout', '_self')
     }
     return (
         <div className='header'>
